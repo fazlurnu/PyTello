5f4bf659f3ea757b372019e3b642e8960065a70d
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-06-18 16:54:15 +0800

    create functions

--- FaceDetection.py
+++ FaceDetection.py
@@ -8,8 +8,7 @@ GREEN = (0, 255, 0)
 BLUE = (255, 0, 0)
 RED = (0, 0, 255)
 
-while True:
-    ret, frame = cap.read()
+def detect_face(frame, display = False):
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     
@@ -28,10 +27,21 @@ while True:
         center_face = (int(x + w/2), int(y + h/2))
         cv.line(frame, center, center_face, RED, 2)
         cv.circle(frame, center_face, 5, RED, 2)
+        
+    if(display):
+        cv.imshow('frame', frame)
+        
+    
+def main():
+    while True:
+        ret, frame = cap.read()
+        detect_face(frame, display=True)
+        
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv.destroyAllWindows()
+if __name__ == "__main__":
+    main()
+    cap.release()
+    cv.destroyAllWindows()
+    
