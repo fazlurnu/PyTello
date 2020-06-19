b9602b8ebb6c506ae629fc4494610c16918ce08f
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-06-19 21:23:38 +0800

    Create main

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 21:02:03 2020
+
+@author: My Laptop
+"""
+
+import DroneController
+
+def main():
+    tello = DroneController.Tello()
+    
+if __name__ == '__main__':
+    main()
