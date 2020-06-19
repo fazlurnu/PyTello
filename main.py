54078cef699ec9695d779255fe6500258dc8859d
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-06-19 21:42:26 +0800

    Non delay condition

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ Created on Fri Jun 19 21:02:03 2020
 import DroneController
 import time
 
+prev_time = time.time()
+
 def main():
     tello = DroneController.Tello()
     
+    curr_time = time.time())
+    interval = 10
     tello.takeoff()
-    time.sleep(1)
-    tello.land()
+    
+    if(curr_time - prev_time > interval):
+        prev_time = curr_time
+        tello.land()
     
 if __name__ == '__main__':
     main()
