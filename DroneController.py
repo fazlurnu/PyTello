6a51d62367df0c431eb1f697b1756058710b1037
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-06-19 21:04:25 +0800

    Create Tello class

---
+++ DroneController.py
@@ -0,0 +1,40 @@
+import socket
+import time
+import logging
+
+logger = logging.getLogger(__name__)
+
+class Tello(object):
+    def __init__(self, controller_ip='192.168.10.2', controller_port=8889,
+                 drone_ip='192.168.10.1', drone_port=8889):
+        self.controller_ip = controller_ip
+        self.controller_port =  controller_port
+        self.controller_address = (self.controller_ip, self.controller_port)
+        self.drone_ip = drone_ip
+        self.drone_port = drone_port
+        self.drone_address = (drone_ip, drone_port)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind(self.controller_address)
+        
+        self.send_command('command') #initialize SDK mode
+        self.send_command('streamon') #initialize video streaming
+        
+        def __del__(self):
+            self.stop_connection()
+            
+        def stop_connection(self):
+            self.socket.close()
+            
+        def send_command(self, command):
+            logger.info({'action': 'send_command', 'command': command})
+            self.socket.sendto(command.encode('utf-8'), self.drone_address)
+            
+        def takeoff(self):
+            self.send_command("takeoff")
+            
+        def land(self):
+            self.send_command("land")
+        
+        
+        
+        
