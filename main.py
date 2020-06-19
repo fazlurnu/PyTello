33ed41957a70766d49beffec3b738702bf3325c6
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-06-19 21:51:16 +0800

    use while loop

--- main.py
+++ main.py
@@ -8,18 +8,20 @@ Created on Fri Jun 19 21:02:03 2020
 import DroneController
 import time
 
-prev_time = time.time()
-
 def main():
     tello = DroneController.Tello()
     
-    curr_time = time.time()
-    interval = 10
+    prev_time = time.time()
+    interval = 5
+    time.sleep(2)
     tello.takeoff()
     
-    if(curr_time - prev_time > interval):
-        prev_time = curr_time
-        tello.land()
+    while True:
+        curr_time = time.time()
+        if(curr_time - prev_time > interval):
+            prev_time = curr_time
+            tello.land()
+            break
     
 if __name__ == '__main__':
     main()
