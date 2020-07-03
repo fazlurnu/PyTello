f05beec963124d004871678517a316a91fa6a85f
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-07-03 10:37:55 +0800

    test TO and LD

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-import DroneController
+from DroneController import Tello
 import time
 import cv2 as cv
 
 def main():
-    tello = DroneController.Tello()
+    tello = Tello()
     
-    bat = int(tello.battery())
+    tello.connect()
     
-    if (bat>30):
-        tello.takeoff()
-        time.sleep(8)        
-        
-        angle = 0
-        for i in range(12):
-            print(angle)
-            tello.rotate_cw(30)
-            angle+=30
-        
-        time.sleep(3)
-        
-        tello.land()
-        tello.stop_connection()
+    tello.takeoff()
+    time.sleep(8)
+    tello.land()
     
 if __name__ == '__main__':
     main()
