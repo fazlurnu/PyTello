c9d5c884351b91139a5c72e34ea766851b706783
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-07-06 20:53:41 +0800

    change function

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -1,16 +1,35 @@
 from Matrix import matrix
 
-def kalman_filter(x, u, P, F, R, H, z):
+def kalman_filter(x, P, z, data_ok = True):
 
+    u = matrix([[]])
+    u = u.zero(x.dimx, x.dimy)
+    
+    dv = 1.05
+    dt = 500
+    
+    F = matrix([[1., dt, 0., 0.],
+                [0., dv, 0., 0.],
+                [0., 0., 1., dt],
+                [0., 0., 0., dv]]) # next state function
+    
+    H = matrix([[1., 0., 0., 0.],
+                [0., 0., 1., 0.]]) # measurement function
+    
+    R = matrix([[1, 0.],
+                [0., 1]]) # measurement uncertainty
+    
     I = matrix([[]])
     I = I.identity(x.dimx)
     
-    #measurement update
-    y = z-H*x
-    S = H*P*H.transpose()+R
-    K = P*H.transpose()*S.inverse()
-    x = x + K*y
-    P = (I-K*H)*P
+    if (data_ok):
+        #measurement update
+        y = z-H*x
+        S = H*P*H.transpose()+R
+        K = P*H.transpose()*S.inverse()
+        x = x + K*y
+        P = (I-K*H)*P
+    
     #prediction
     x = F*x + u;
     P = F*P*F.transpose()
@@ -29,29 +48,17 @@ def main():
                 [0.],
                 [0.]])
 
-    u = matrix([[]])
-    u = u.zero(x.dimx, x.dimy)
-    
     P = matrix([[50., 0., 0., 0.],
                 [0., 50., 0., 0.],
                 [0., 0., 50., 0.],
                 [0., 0., 0., 50.]]) # initial uncertainty
-    
-    F = matrix([[1., 1., 0., 0.],
-                [0., 1., 0., 0.],
-                [0., 0., 1., 1.],
-                [0., 0., 0., 1.]]) # next state function
-    H = matrix([[1., 0., 0., 0.],
-                [0., 0., 1., 0.]]) # measurement function
-    
-    R = matrix([[1., 0.],
-                [0., 1.]]) # measurement uncertainty
 
     for i in range(len(measurements)):
         z = matrix(measurements[i])
-        x, P = kalman_filter(x, u, P, F, R, H, z)
+        x, P = kalman_filter(x, P, z)
         
-    print(x)
+    print("states: " + str(x))
+    print("covariance: " + str(P))
 
 if __name__ == "__main__":
     main()
