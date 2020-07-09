84de3873d433885dbd28fe541cd0ba83d8e41877
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-07-09 11:59:47 +0800

    Revert "remove matrix and KF"
    This reverts commit 9615b543c911c59f581ac60f6f373c32358ebf7b.

---
+++ KalmanFilter.py
@@ -0,0 +1,63 @@
+from Matrix import matrix
+
+def kalman_filter(x, P, z, dt, data_ok = True):
+
+    u = matrix([[]])
+    u = u.zero(x.dimx, x.dimy)
+    
+    dv = 1.001
+    
+    F = matrix([[1., dt, 0., 0.],
+                [0., dv, 0., 0.],
+                [0., 0., 1., dt],
+                [0., 0., 0., dv]]) # next state function
+    
+    H = matrix([[1., 0., 0., 0.],
+                [0., 0., 1., 0.]]) # measurement function
+    
+    R = matrix([[0.1, 0.],
+                [0., 0.1]]) # measurement uncertainty
+    
+    I = matrix([[]])
+    I = I.identity(x.dimx)
+    
+    if (data_ok):
+        #measurement update
+        y = z-H*x
+        S = H*P*H.transpose()+R
+        K = P*H.transpose()*S.inverse()
+        x = x + K*y
+        P = (I-K*H)*P
+    
+    #prediction
+    x = F*x + u;
+    P = F*P*F.transpose()
+        
+    return x,P
+
+def main():
+    
+    measurements = [[[1.0],[2.0]],
+                    [[2.0],[4.0]],
+                    [[3.0],[6.0]]]
+
+    
+    x = matrix([[0.],
+                [0.],
+                [0.],
+                [0.]])
+
+    P = matrix([[50., 0., 0., 0.],
+                [0., 50., 0., 0.],
+                [0., 0., 50., 0.],
+                [0., 0., 0., 50.]]) # initial uncertainty
+
+    for i in range(len(measurements)):
+        z = matrix(measurements[i])
+        x, P = kalman_filter(x, P, z)
+        
+    print("states: " + str(x))
+    print("covariance: " + str(P))
+
+if __name__ == "__main__":
+    main()
