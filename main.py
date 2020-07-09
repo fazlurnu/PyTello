f9abb2631a5197bc2d318e4218eb8756f3812c14
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-07-09 16:53:20 +0800

    add logging

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from FaceDetection import detect_face
 import time
 import cv2 as cv
 
+kpX = 0.11
+kpY = 0.15
+title = str("kpX= " + str(kpX) + ", kpY= " + str(kpY) + ".txt")
+
 cap = cv.VideoCapture(0)
 
 tello = Tello()
@@ -12,17 +16,34 @@ bat = tello.query_battery()
 tello.takeoff()
 tello.move_up(10)
 
+start_time = time.time()
+
 while True:
+    time_pure = time.time() - start_time
+    file1 = open(title,"a")
+    
     frame = tello.get_frame_read().frame  # capturing frame from drone 
     
     diff_x, diff_y = detect_face(frame)
     
     cv.imshow("drone", frame)             
     
-    tello.send_rc_control(int(diff_x/4), 0, int(-diff_y/4), 0)
+    controlX = diff_x * kpX
+    controlY = diff_y * kpY
+    
+    if (controlX > 35):
+        controlX = 35
+    elif (controlX < -35):
+        controlX = -35
+        
+    tello.send_rc_control(int(controlX), 0, 0, 0)
+    
+    file1.write(str(time_pure) + ". " + str(diff_x) + "," + str(controlX) + "\n")
+    file1.close()
     
     if cv.waitKey(1) & 0xFF == ord('q'):  # quit from script
         cap.release()
         cv.destroyAllWindows()
+        tello.streamoff()
         tello.land()
         break
