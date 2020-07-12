9a08fdef50c018cf14b52c0304c1a5bcdd107512
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-07-12 12:23:59 +0800

    change control to yaw

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from FaceDetection import detect_face
 import time
 import cv2 as cv
 
-kpX = 0.11
-kpY = 0.15
+kpX = 0.15
+kpY = 0.3
 title = str("log/kpX= " + str(kpX) + ", kpY= " + str(kpY) + ".txt")
-
-cap = cv.VideoCapture(0)
 
 tello = Tello()
 
@@ -42,14 +40,13 @@ while True:
     controlX = set_limit(controlX, -35, 35)
     controlY = set_limit(controlY, -35, 35)
         
-    tello.send_rc_control(int(controlX), 0, 0, 0)
+    tello.send_rc_control(0, 0, int(-controlY), int(controlX))
     
-    file1.write(str(time_pure) + ". " + str(diff_x) + "," + str(controlX) + "\n")
+    file1.write(str(time_pure) + ", " + str(diff_x) + "," + str(controlX) + "\n")
     file1.close()
     
     if cv.waitKey(1) & 0xFF == ord('q'):  # quit from script
-        cap.release()
-        cv.destroyAllWindows()
         tello.streamoff()
         tello.land()
+        cv.destroyAllWindows()
         break
