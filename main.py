200972363391010ab1ed2965fefaf22ce9521ab0
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-07-14 21:25:55 +0800

    do yaw for TO

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 from DroneController import Tello
 from FaceDetection import detect_face
 import cv2 as cv
+import time
 
 kpX = 0.15
 kpY = 0.3
 
 tello = Tello()
 
-bat = tello.query_battery()
+#tello.streamon()
+bat = tello.get_battery()
 
+print(bat)
+
+is_takeoff = False
+counter = 0
+
+while not is_takeoff:
+    print(tello.get_yaw(), counter)
+    if tello.get_yaw() < -30:
+        counter += 1
+        time.sleep(1)
+        
+    if counter > 3:
+        is_takeoff=True
+        print("ready")
+        time.sleep(5)
+    
+tello.streamon()
 tello.takeoff()
 tello.move_up(10)
-        
+    
 while True:
     
     frame = tello.get_frame_read().frame  # capturing frame from drone 
