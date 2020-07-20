0d17d8bc1133e400d951dab377d11625da32a1b5
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-07-20 22:47:34 +0800

    create dummy class

---
+++ MyDrone.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 20 22:42:17 2020
+
+@author: My Laptop
+"""
+
+class MyDrone:
+    
+    def __init__(self):
+        
+        print("hello world")
+        
+    def print_here(self):
+        print("I'm here")
