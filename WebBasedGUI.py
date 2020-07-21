881f0a1bd262673d36af38f281b64fb977c03da7
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-07-21 16:23:59 +0800

    add connect

--- WebBasedGUI.py
+++ WebBasedGUI.py
@@ -3,15 +3,7 @@ from imutils.video import VideoStream
 from flask import Response
 from flask import Flask
 from flask import render_template
-
-from MyDrone import MyDrone
-
-import threading
-import argparse
-import datetime
-import imutils
-import time
-import cv2
+from DroneController import Tello
 
 app = Flask(__name__)
 
@@ -19,26 +11,30 @@ is_flying = False
 
 @app.route("/")
 def home():
-    myDrone = MyDrone()
-    myDrone.print_here()
     return render_template("index.html")
     
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route('/background_process_test')
-def background_process_test():
+@app.route('/TO_LD')
+def TO_LD():
     global is_flying
     
     if (is_flying):
         print("Landing")
+        
     else:
         print("Take off")
 
     is_flying = not(is_flying)
-    
+
     #print ("Drone is flying: " + str(is_flying))
+    return ("nothing")
+
+@app.route('/connect')
+def connect():
+    print("connect")
     return ("nothing")
     
 if __name__ == "__main__":
